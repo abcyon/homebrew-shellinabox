046b74519dd7fe7737846d2a2469a33c749a7852
성동찬 <chan@seongdongchan-ui-MacStudio.local>
2026-03-20 11:32:19 +0900

    Fix macOS build: add _DARWIN_C_SOURCE for strdup/snprintf

--- Formula/shellinabox.rb
+++ Formula/shellinabox.rb
@@ -13,6 +13,7 @@ class Shellinabox < Formula
   def install
     system "autoreconf", "-fiv"
     system "./configure",
+           "CFLAGS=-D_DARWIN_C_SOURCE #{ENV.optflags}",
            "--disable-debug",
            "--disable-dependency-tracking",
            "--disable-silent-rules",
