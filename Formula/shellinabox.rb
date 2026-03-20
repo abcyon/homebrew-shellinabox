6825a4b02a984fbf78ca16ba10ff58903057af02
성동찬 <chan@seongdongchan-ui-MacStudio.local>
2026-03-20 10:51:37 +0900

    Add shellinabox formula v2.20.1

---
+++ Formula/shellinabox.rb
@@ -0,0 +1,35 @@
+class Shellinabox < Formula
+  desc "Web-based terminal emulator using AJAX/Comet"
+  homepage "https://github.com/abcyon/shellinabox"
+  url "https://github.com/abcyon/shellinabox/archive/refs/tags/v2.20.1.tar.gz"
+  sha256 "7286379d2b39f9e75e72cfa307ff279ff6173f0ca7059ae15c74ddb1e8ecd806"
+  license "GPL-2.0-only"
+  head "https://github.com/abcyon/shellinabox.git", branch: "master"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+
+  def install
+    system "autoreconf", "-fiv"
+    system "./configure",
+           "--disable-debug",
+           "--disable-dependency-tracking",
+           "--disable-silent-rules",
+           "--prefix=#{prefix}",
+           "--mandir=#{man}",
+           "--sysconfdir=#{etc}"
+    system "make"
+    system "make", "install"
+  end
+
+  service do
+    run [opt_sbin/"shellinaboxd", "--port=4200", "--no-beep"]
+    keep_alive true
+    log_path var/"log/shellinabox.log"
+    error_log_path var/"log/shellinabox.log"
+  end
+
+  test do
+    system "#{sbin}/shellinaboxd", "--help"
+  end
+end
